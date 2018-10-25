d384518a62a40d7b0e4bfb3e31f848d19257d4d6
Farzeen Najam <farzeennajam@Farzeens-MacBook-Pro.local>
2018-10-24 21:39:27 -0400

    hate this

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,5 +1,8 @@
 
 public class LinkStrand implements IDnaStrand {
+	int myIndex;
+	int myLocalIndex;
+	Node myCurrent;
 
 	private class Node {
 		String info;
@@ -39,7 +42,13 @@ public class LinkStrand implements IDnaStrand {
 		myLast = myFirst; // initialize mylast as well. point to same node at first then mylast iterates
 							// overnodes.
 		myAppends = 0;
-		mySize = source.length(); // REMEMBER TO ADD SOURCE LENGTH AS WELL,ORIGINAL LENGTH.
+		mySize = source.length();
+
+		Node myCurrent = myFirst;
+		myLocalIndex = 0;
+		myIndex = 0;
+
+		// REMEMBER TO ADD SOURCE LENGTH AS WELL,ORIGINAL LENGTH.
 
 	}
 
@@ -63,40 +72,83 @@ public class LinkStrand implements IDnaStrand {
 		// TODO Auto-generated method stub
 		return this;
 	}
+	
+	 private Node pointer=null;
+
+	private Node appendFirst(String s) {
+	    	
+	    	
+	    	//if(pointer!=null) {
+	    		StringBuilder copy=new StringBuilder(s);
+	    		copy.reverse();
+	    		
+	            Node tail=new Node(copy.toString());
+	            //creates a new node and append it to the myLast node
+	            tail.next=pointer;
+	            pointer=tail;
+	            //tail1=tail1.next;
+	        	//}
+	            return pointer;
+	        }
 
 	@Override
 	public IDnaStrand reverse() {
-		StringBuilder copy = new StringBuilder(myFirst.info); // temperory holder
-		copy.reverse();
-		LinkStrand org = new LinkStrand(copy.toString()); // has one node already
-		Node last1 = org.myLast;
-		Node pointer = org.myFirst; // a pointer in reverse dna strand
+/*//		StringBuilder copy = new StringBuilder(myFirst.info); // temperory holder
+//		copy.reverse();
+//		LinkStrand org = new LinkStrand(copy.toString()); // has one node already
+		Node last1 = myLast;
+		Node pointer = myFirst;
+		Node first1 = myFirst;// a pointer in reverse dna strand
 		myFirst = myFirst.next;
 
-		while (myFirst != null) {
-			StringBuilder copy1 = new StringBuilder(myFirst.info); // temperory holder
+		while (first1 != null) {
+			StringBuilder copy1 = new StringBuilder(first1.info); // temperory holder
 			copy1.reverse(); // Node two = new Node(copy1.toString());
-			org.append(copy1.toString());
+			first1.info = copy1.toString();
+
 			// LinkStrand other = new LinkStrand(copy1.toString());
 
-			myFirst = myFirst.next;
+			first1 = first1.next;
 		}
 
 		Node prev = null;
 
 		while (pointer != null) {
-			Node temp = pointer.next; //creates a temporary node that stores the rest of loop.
-			pointer.next = prev;// before we can assign pointer the value of next.
-			  pointer = temp; //now the pointer moves to temp.
-			 
-			  last1 = pointer; //now my last1 is the pointer.
+			// System.out.println(pointer);
+			Node temp = pointer.next;
+			pointer.next = prev;
+			prev = pointer;
+			pointer = temp; // now the pointer moves to temp.
 
-			// org.next = org;
+			myFirst = pointer; // now my last1 is the pointer.
 
-		}
+		}*/
+		
+		    
+		  
+		  
+		    	
+		      Node head=myFirst; 
+		      while(head.next!=null) {
+		    	  appendFirst(head.info);
+		    	  head=head.next;
+		      }
+		      
+		   appendFirst(head.info);
+		   LinkStrand ans=new LinkStrand(""); 
+		   
+		   while(pointer!=null) {
+			   ans.append(pointer.info);
+			   pointer=pointer.next;
+		   }
+		   
+		   return ans;
+		    }
+		
+		
 
-		return org;
-	}
+
+	
 
 	/**
 	 * @param
@@ -110,9 +162,51 @@ public class LinkStrand implements IDnaStrand {
 
 	@Override
 	public char charAt(int index) {
+		if(index < 0) {
+			throw new IndexOutOfBoundsException("");
+		}
+		
+		
+		
+		while (myIndex != index) {
+			myIndex++;
+			myLocalIndex++;
+			
+			if (myLocalIndex >= myCurrent.info.length()) {
+				myLocalIndex = 0;
+				myCurrent = myCurrent.next;
+			
+			}
+		}
+
+
+		/*while (myIndex != index) {
+
+			if (myLocalIndex >= myCurrent.info.length()) {
+				if(myCurrent.next != null) {
+					myLocalIndex = -1;
+				}
+				else if(myCurrent.next ==null) {
+					throw new IndexOutOfBoundsException("");
+
+
+				}
+
+			}
+
+
+			myCurrent = myCurrent.next;
+
+		}
+
+		myIndex++;
+		myLocalIndex++;
+*/
+
+		return myCurrent.info.charAt(myLocalIndex);
 
 		// TODO Auto-generated method stub
-		return 0;
+
 	}
 
 	public String toString() {
